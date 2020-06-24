d0794c428d552fd8aaee292df1634897623cd412
Marcela Guerrero <marcelaguerrero1396@gmail.com>
2020-06-24 10:02:41 -0500

    Agregando preventDefault

--- js/funciones.js
+++ js/funciones.js
@@ -1,5 +1,6 @@
 $(document).ready(function() {
     $('#formRegistroAutos').submit(function(e) {
+        e.preventDefault();
         $.ajax({
             type: "POST", // metodo por el cual se envian los datos
             data: $("#formRegistroAutos").serialize(), // Se toman todos los datos del formulario
